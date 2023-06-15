c616de83a37d538d43c2197b8043f0888c1f3141
Alias Qli <2576814881@qq.com>
2023-06-15 22:16:13 +0800

    tyck: fix test example

--- base/src/test/java/DeclsTest.java
+++ base/src/test/java/DeclsTest.java
@@ -77,7 +77,7 @@ public class DeclsTest {
 
   @Test public void partialTest() {
     tyck("""
-      def t1 (i j : I) (φ : F) : Partial 1 = 1, F => {| φ := (forall j => j = 0) ∨ (¬ j = 0) | (i = 1) := φ ∧ (j = 1) |}
+      def partial (i j : I) (φ : F) : Partial (φ ∨ i = 1), F => {| φ := (forall j => j = 0) ∨ (¬ j = 0) | (i = 1) := φ ∧ (j = 1) |}
     """);
   }
 
