11909037eec1defa51698e778652febb5da3f485
Alias Qli <2576814881@qq.com>
2023-06-20 17:06:54 +0800

    tyck: fix arg order

--- base/src/main/java/org/aya/lamett/syntax/Term.java
+++ base/src/main/java/org/aya/lamett/syntax/Term.java
@@ -174,16 +174,16 @@ public sealed interface Term extends Docile {
   record Partial(@NotNull Term cofib, @NotNull Term type) implements Term {}
   record PartEl(@NotNull ImmutableSeq<Tuple2<Cofib.Conj, Term>> elems) implements Term {}
   record Coe(@NotNull Term r, @NotNull Term s, @NotNull Term A) implements Term {
-    public @NotNull Coe update(@NotNull Term type, @NotNull Term r, @NotNull Term s) {
-      return A == A() && r == r() && s == s() ? this : new Coe(type, r, s);
+    public @NotNull Coe update(@NotNull Term r, @NotNull Term s, @NotNull Term A) {
+      return A == A() && r == r() && s == s() ? this : new Coe(r, s, A);
     }
 
     public @NotNull Coe descent(@NotNull UnaryOperator<Term> f, @NotNull UnaryOperator<Pat> g) {
-      return update(f.apply(this.A), f.apply(r), f.apply(s));
+      return update(f.apply(r), f.apply(s), f.apply(this.A));
     }
 
     public @NotNull Coe inverse(Term newTy) {
-      return new Coe(newTy, s, r);
+      return new Coe(s, r, newTy);
     }
 
     /**
@@ -196,12 +196,12 @@ public sealed interface Term extends Docile {
      */
     public static @NotNull Lam cover(LocalVar x, Param<Term> A, Term B, Term arg, Term r) {
       var innerCover = Normalizer.rename(new Lam(x, A.type()));
-      var coeRX = new App(new Coe(innerCover, r, new Ref(x)), arg);
+      var coeRX = new App(new Coe(r, new Ref(x), innerCover), arg);
       return new Lam(x, B.subst(A.x(), coeRX));
     }
 
     public @NotNull Coe recoe(Term cover) {
-      return new Coe(cover, r, s);
+      return new Coe(r, s, cover);
     }
 
     public @NotNull Term family() {
@@ -211,8 +211,6 @@ public sealed interface Term extends Docile {
 
   /** Let A be argument, then <code>A i -> A j</code> */
   static @NotNull Pi familyI2J(Term term, Term i, Term j) {
-    return new Pi(
-      new Param<>(new LocalVar("_"), new App(term, i)),
-      new App(term, j));
+    return (Pi) mkPi(new App(term, i), new App(term, j));
   }
 }
